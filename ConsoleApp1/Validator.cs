f9e778e4c1cd91c74c175362f9b3bf1c31e7feb5
Sasha <sashapat1932@gmail.com>
2026-04-08 18:08:37 +0300

    add validation

--- ConsoleApp1/Validator.cs
+++ ConsoleApp1/Validator.cs
@@ -1,6 +1,9 @@
 ﻿namespace Order_processing_system;
 
-public class Validator
+public static class Validator
 {
-    
+    public static bool Validate(Order order)
+    {
+        return order.Quantity > 0 && order.Price > 0;
+    }
 }
